8ea2107fbeecc82df3326ce7e585890dc4adad23
Lucas Bastos <lucsbasto@gmail.com>
2019-09-27 17:04:37 -0300

    adicionando dismissible

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/item.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,17 +27,97 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = List<Item>();
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: false));
+    items.add(Item(title: "Item 3", done: false));
+    items.add(Item(title: "Item 4", done: false));
+    items.add(Item(title: "Item 5", done: false));
+    items.add(Item(title: "Item 6", done: false));
+    items.add(Item(title: "Item 7", done: false));
+  }
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    if (newTaskCtrl.text.isEmpty) return;
+    setState(() {
+      widget.items.add(
+        Item(
+          title: newTaskCtrl.text,
+          done: false,
+        ),
+      );
+    });
+    newTaskCtrl.clear();
+  }
+
+  void remove(int index) {
+    setState(() {
+      print(widget.items[index]);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('TODO LIST'),
-        ),
-        body: Container(
-          child: Center(
-            child: Text("Olá"),
+      appBar: AppBar(
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
           ),
-        ));
+          decoration: InputDecoration(
+            labelText: "Novo Item",
+            labelStyle: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = widget.items[index];
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+              },
+            ),
+            key: Key(item.title),
+            background: Container(
+              color: Colors.red.withOpacity(0.2),
+            ),
+            onDismissed: (direction) {
+              if (direction == DismissDirection.endToStart) print(direction);
+              if (direction == DismissDirection.startToEnd) print(direction);
+              setState(() {
+                print(index);
+                widget.items.removeAt(index);
+              });
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          add;
+        },
+        child: Icon(Icons.add_box),
+        backgroundColor: Colors.pink,
+      ),
+    );
   }
 }
--- lib/models/item.dart
+++ lib/models/item.dart
@@ -3,6 +3,15 @@ class Item {
   bool done;
 
   Item({this.title, this.done});
-}
 
-var item = new Item(title: '', done: true);
+  Item.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    done = json['done'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['done'] = this.done;
+  }
+}
